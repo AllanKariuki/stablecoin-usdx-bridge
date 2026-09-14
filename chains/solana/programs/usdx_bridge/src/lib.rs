39209bdd6f60fbcdba7fa4852e619248c5c609cc
Allan Kariuki <alankariuki24@gmail.com>
2026-09-14 16:01:03 +0300

    continuation of initialization of the solana smart contract using anchor

---
+++ chains/solana/programs/usdx_bridge/src/lib.rs
@@ -0,0 +1,25 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("9GFGtYxGtZg8Smk4yyya7zAT6V3WcLDsB9XWWQbxMS8");
+
+#[program]
+pub mod usdx_bridge {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        crate::instructions::initialize::handle_initialize(ctx)
+    }
+
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        crate::instructions::increment::handle_increment(ctx)
+    }
+}
